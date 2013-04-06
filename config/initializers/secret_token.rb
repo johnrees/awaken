2b4f79150e2373f89c1928cc4d5b4b5b3bbda78b
johnrees <john@bitsushi.com>
2013-04-06 02:50:06 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awaken::Application.config.secret_token = '09af374be1bef2a81d9c587ebb0586da9e6a33ef4483f95860eec9923fa79767fc78259ffbeec48c9dcc91fd36f51597848e46ab8b82b0de58296cb95d0b7693'
