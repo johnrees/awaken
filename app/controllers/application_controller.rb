39377bd6f599040bb06396f0062c537fbbb3baec
johnrees <john@bitsushi.com>
2013-04-07 14:42:43 +0200

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  if Rails.env.production?
+    http_basic_authenticate_with name: ENV['HTTP_USER'],
+      password: ENV['HTTP_PASSWORD']
+  end
 end
