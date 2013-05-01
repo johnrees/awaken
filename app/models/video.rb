37fd8a78763b7eb90e0cd0c9cde3399cc0cd53f1
johnrees <john@bitsushi.com>
2013-05-01 15:59:59 +0200

    admin fix

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   attr_accessible :client, :kind, :name, :attachment, :zencoder_output_id, :processed, :thumbnail
-  validates_presence_of :client, :kind, :name, :attachment
+  validates_presence_of :client, :kind, :name, :attachment, on: :create
   mount_uploader :attachment, VideoUploader
 
   def to_s
