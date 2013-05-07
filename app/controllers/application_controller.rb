4c19ed9bedc5c8e464f05555b62b31424d47ad04
johnrees <john@bitsushi.com>
2013-05-07 08:59:42 +0200

    auth again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout proc {|controller| controller.request.xhr? ? false: "application" }
+  # http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
+
   before_filter :authenticate
 
-private
-
   def authenticate
-    if Rails.env.production?
-      http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
+    authenticate_or_request_with_http_basic('Secret') do |username, password|
+      username == ENV['HTTP_USER'] && password == ENV['HTTP_PASSWORD']
     end
   end
 end
