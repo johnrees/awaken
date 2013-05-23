3f972f8b27e590d8588ab847a2536eca5c8e8a76
johnrees <john@bitsushi.com>
2013-05-23 12:57:15 +0200

    public image uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,9 +11,9 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV['AWS_S3_SECRET'],
       :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
     }
-    config.asset_host = "http://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com"
-    config.fog_directory = ENV['AWS_S3_BUCKET']
-    config.fog_public     = false
+    config.asset_host     = "http://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com"
+    config.fog_directory  = ENV['AWS_S3_BUCKET']
+    config.fog_public     = true
     config.fog_attributes = {
       'Cache-Control'=>'max-age=315576000'
       # 'access-control-allow-origin' => '*',
